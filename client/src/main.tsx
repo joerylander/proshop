763ba2f46c285206aa84689f6fdbe2416b692e0f
Joe Rylander <rylander.j92@gmail.com>
2026-04-29 12:36:30 +0200

    feat: add ProductScreen component and update routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,14 +8,15 @@ import {
 } from 'react-router-dom';
 import './assets/styles/index.css';
 import './assets/styles/bootstrap.custom.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.tsx';
 import HomeScreens from './screens/HomeScreens.tsx';
+import ProductScreen from './screens/ProductScreen.tsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="/" index={true} element={<HomeScreens />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>,
   ),
 );
